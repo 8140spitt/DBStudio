2f479978d9e5be4a3bc77d84a0ed353aff999098
Stephen Spittal <stevejtspittal@gmail.com>
2025-05-23 02:37:08 +0100

    6

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,31 +1,10 @@
-import { getSession } from '$lib/server/actions/auth/index';
+
 import type { Handle } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 
-    const sessionToken = event.cookies.get('dbstudio-session');
 
-    if (event.url.pathname.startsWith('/auth')) {
-        const response = await resolve(event);
-        return response;
-    }
-
-    if (!sessionToken) {
-        event.locals.user = null;
-        event.locals.session = null;
-        redirect(302, '/auth/login');
-    }
-    const { session, user } = await getSession(sessionToken);
-
-    if (session) {
-        event.locals.user = user;
-        event.locals.session = session;
-    } else {
-        event.locals.user = null;
-        event.locals.session = null;
-        redirect(302, '/auth/login');
-    }
 
     const response = await resolve(event);
     return response;
--- src/routes/auth/register/onboarding/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	let { data } = $props();
-</script>
-
-{data.userId}
-{JSON.stringify(data.user)}
